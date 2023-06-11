fa0d8d404ebd238dd0b838d6fa3209fc69ecc0c1
Ed Benedict Quia <edbenedictquia@gmail.com>
2023-06-11 12:49:22 +0800

    add pseudocode

---
+++ script.js
@@ -0,0 +1,11 @@
+// create functions for the following items: add, subtract, multiply, divide
+// calculation operation will consist of a Number, operator, and another Number
+// create a variable for the first Number
+// create a variable for the operator
+// create a variable for the second Number
+// create a function operate that takes the operator, first Number, and second number and call a function from the top
+// create a function that will populate the display when you click the numbers button
+// store the display value
+// store the first number that is input into the calculator when a user presses an operator
+// save the operator that was pressed
+// operate when user pressed the '=' key or when user pressed another operator
