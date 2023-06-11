552f6876a6b33c3f377e2fa01ee763e2580bac83
Ed Benedict Quia <edbenedictquia@gmail.com>
2023-06-11 11:47:50 +0800

    add math functions

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // store the numbers entered by the users
 // add function
+function add(a, b) {
+    return a + b;
+}
+
 // substract function
+function substract(a, b) {
+    return a - b;
+}
+
 // multiply function
+function multiply(a, b) {
+    return a * b;
+}
+
 // divide function
+function divide(a, b) {
+    return a / b;
+}
+
 // operate function
 
